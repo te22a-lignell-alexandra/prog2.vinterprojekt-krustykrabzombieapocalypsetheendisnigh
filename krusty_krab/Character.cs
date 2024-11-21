c118bf0349fc4c60b41edc40af62f15b73588f41
Alexandra Lignell <alexandra.lignell@elev.ga.ntig.se>
2024-11-21 16:00:58 +0100

    ändrade en liten sak

--- krusty_krab/Character.cs
+++ krusty_krab/Character.cs
@@ -6,18 +6,22 @@ public class Character
     private int infectionLevel;
     private int hit;
     private int dodge;
-    public Character(string Name, int Hp)
+    public Character(string Name, int Hp, int InfectionLevel, int Hit, int Dodge)
     {
         name = Name;
         hp = Hp;
+        infectionLevel = InfectionLevel;
+        hit = Hit;
+        dodge = Dodge;
         currentHp = hp;
+
     }
-    public bool Attack(int hit, int dodge)
+    public bool Attack(int dodge)
     {
         bool success;
         int Hit = Random.Shared.Next(0, hit);
         int Dodge = Random.Shared.Next(0, dodge);
-        
+
         if (Hit > Dodge) return success = true;
         else return success = false;
     }
